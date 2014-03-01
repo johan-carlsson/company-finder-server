94de28760ec99cb7d4ce817465d6d44c3a8fe9bb
Johan Carlsson <johancarlsson@me.com>
2014-03-01 10:45:16 +0100

    Added generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompanyFinderServer::Application.config.secret_key_base = 'b73881a6d4b320fb5b2fbbd41dbae03e808b23b675c869d8235b2aef061bfa6e6dd1aa2496d4cbf17c9e928844d36ae0ddf856bf5698117f0ceb21c166e360be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompanyFinderServer::Application.config.session_store :cookie_store, key: '_company-finder-server_session'
