6938f5d0093aea198a0eab11610357bd0a235272
Johan Carlsson <johancarlsson@me.com>
2014-03-01 19:53:27 +0100

    Implemented company_backends scrape functionality Added nokogiri gem

--- app/services/company_backend.rb
+++ app/services/company_backend.rb
@@ -1,8 +1,47 @@
+require 'open-uri'
 class BackendError < RuntimeError;end
 
 class CompanyBackend
+  BASE_URL="http://www.allabolag.se"
+
   def self.find_company_by_name(name)
-    #Dummy
-    Company.new(name,"1") 
+    begin
+      identity=self.scrape_identity(name)
+    rescue => e
+      Rails.logger.error("ERROR: Failed to scrape identity for: #{name} excpetion: #{e}")
+      raise BackendError, e
+    end
+
+    if identity
+      Rails.logger.info("CompanyBackend found: #{identity} for: #{name}")
+      company=Company.new(name,identity) 
+    else
+      Rails.logger.info("CompanyBackend could not find: #{name}")
+    end
+
+    return company
   end  
+
+  def self.scrape_identity(name)
+    uri_for(name).open {|f|
+      parse(f.read)
+    }
+  end
+
+  def self.uri_for(name)
+    URI.parse(URI.escape("#{BASE_URL}/?what=#{name}"))
+  end
+
+  # <td class="text11grey6">
+  #  <span class="bold11grey6">Org.nummer:</span> 556641-0576<br>
+  #  <span class="bold11grey6">Verksamhet:</span> Utgivning av annan programvara<br>
+  # </td>
+  def self.parse(html)
+    page=Nokogiri::HTML(html)
+
+    #This ain't pretty
+    identity_node=page.xpath("//span[text()='Org.nummer:']/../text()")[1]
+    identity_node.text.strip if identity_node
+  end
+
 end
---
+++ test/services/company_backend_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'minitest/mock'
+
+class CompanyBackendTest < ActiveSupport::TestCase
+  HTML=File.open("#{Rails.root}/test/fixtures/Redpill.html").read 
+
+  test "parse should find first occurrence of identity from response" do
+    assert_equal "556641-0576",CompanyBackend.parse(HTML)
+  end
+
+  test "parse should return nil if no occurrence of identity is found" do
+    assert_nil CompanyBackend.parse("Nothing to be found here") 
+  end
+
+  test "uri_for should escape uri" do
+    assert_equal "http://www.allabolag.se/?what=R%20R",CompanyBackend.uri_for("R R").to_s
+  end
+
+  test "should find company by name" do
+    company=Company.new("A company","123-456")
+    CompanyBackend.stub :scrape_identity, "123-456" do
+      result=CompanyBackend.find_company_by_name("A company")
+      assert_equal company.to_s, result.to_s
+    end
+  end
+
+end
