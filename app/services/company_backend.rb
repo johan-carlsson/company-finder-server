4adb494f655deda096f4b0b751fb3a101fb6035d
Johan Carlsson <johancarlsson@me.com>
2014-03-02 17:55:30 +0100

    Tightened up CompanyBackend by making most class methods private

--- app/services/company_backend.rb
+++ app/services/company_backend.rb
@@ -3,43 +3,46 @@ class BackendError < RuntimeError;end
 
 class CompanyBackend
   BASE_URL="http://www.allabolag.se"
-
-  def self.find_company_by_name(name)
-    begin
-      if(found=self.scrape_for_identity(name))
-        Rails.logger.info("CompanyBackend found: #{found} for: #{name}")
-        company=Company.new(name,found) 
-      else
-        Rails.logger.info("CompanyBackend could not find: #{name}")
+  class << self
+    def find_company_by_name(name)
+      begin
+        if(found=scrape_for_identity(name))
+          Rails.logger.info("CompanyBackend found: #{found} for: #{name}")
+          company=Company.new(name,found) 
+        else
+          Rails.logger.info("CompanyBackend could not find: #{name}")
+        end
+      rescue => e
+        Rails.logger.error("ERROR: Failed to scrape identity for: #{name} excpetion: #{e}")
+        raise BackendError, e
       end
-    rescue => e
-      Rails.logger.error("ERROR: Failed to scrape identity for: #{name} excpetion: #{e}")
-      raise BackendError, e
+
+      return company
+    end  
+
+    private 
+
+    def scrape_for_identity(name)
+      uri_for(name).open do |f|
+        parse(f.read)
+      end
     end
 
-    return company
-  end  
-
-  def self.scrape_for_identity(name)
-    uri_for(name).open do |f|
-      parse(f.read)
+    def uri_for(name)
+      URI.parse(URI.escape("#{BASE_URL}/?what=#{name}"))
     end
+
+    # <td class="text11grey6">
+    #  <span class="bold11grey6">Org.nummer:</span> 556641-0576<br>
+    #  <span class="bold11grey6">Verksamhet:</span> Utgivning av annan programvara<br>
+    # </td>
+    def parse(html)
+      page=Nokogiri::HTML(html)
+
+      # This ain't pretty
+      identity_node=page.xpath("//span[text()='Org.nummer:']/../text()")[1]
+      identity_node.text.strip if identity_node
+    end
+
   end
-
-  def self.uri_for(name)
-    URI.parse(URI.escape("#{BASE_URL}/?what=#{name}"))
-  end
-
-  # <td class="text11grey6">
-  #  <span class="bold11grey6">Org.nummer:</span> 556641-0576<br>
-  #  <span class="bold11grey6">Verksamhet:</span> Utgivning av annan programvara<br>
-  # </td>
-  def self.parse(html)
-    page=Nokogiri::HTML(html)
-
-    # This ain't pretty
-    identity_node=page.xpath("//span[text()='Org.nummer:']/../text()")[1]
-    identity_node.text.strip if identity_node
-  end
-
 end
--- test/services/company_backend_test.rb
+++ test/services/company_backend_test.rb
@@ -5,15 +5,15 @@ class CompanyBackendTest < ActiveSupport::TestCase
   HTML=File.open("#{Rails.root}/test/fixtures/Redpill.html").read 
 
   test "parse should find first occurrence of identity from response" do
-    assert_equal "556641-0576",CompanyBackend.parse(HTML)
+    assert_equal "556641-0576",CompanyBackend.send(:parse,HTML)
   end
 
   test "parse should return nil if no occurrence of identity is found" do
-    assert_nil CompanyBackend.parse("Nothing to be found here") 
+    assert_nil CompanyBackend.send(:parse,"Nothing to be found here") 
   end
 
   test "uri_for should escape uri" do
-    assert_equal "http://www.allabolag.se/?what=R%20R",CompanyBackend.uri_for("R R").to_s
+    assert_equal "http://www.allabolag.se/?what=R%20R",CompanyBackend.send(:uri_for,"R R").to_s
   end
 
   test "should find company by name" do
